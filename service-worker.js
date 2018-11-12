b1e53fa39e42128ce1fc60b7837e58d1863a2e66
vetoshk1n <olvetoshkin@gmail.com>
2018-11-13 01:52:14 +0200

    add Service Worker

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('randomquote').then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/src/main.js',
+        '/src/index.css',
+        'https://use.fontawesome.com/releases/v5.4.1/css/all.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  const url = event.request.url;
+  if (url.startsWith('https://andruxnet-random-famous-quotes.p.mashape.com/') || 
+      url.startsWith('http://quotes.rest/qod')) {
+    if (!navigator.onLine){
+      event.respondWith(new Response({
+        status: 200
+      }));
+      return; 
+    }
+  }
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
